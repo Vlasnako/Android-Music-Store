96749fd472bc26cfcfc2d7657beabe0beb061d22
Vlasnako <vlas.nako@gmail.com>
2023-11-05 14:47:14 +0200

    added Intent

---
+++ MusicStore/app/src/main/java/com/example/musicstore/MainActivity.java
@@ -0,0 +1,111 @@
+package com.example.musicstore;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Spinner;
+import android.widget.TextView;
+
+import java.util.Map;
+
+
+/** @noinspection DataFlowIssue*/
+public class MainActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
+    int quantity;
+    Spinner spinner;
+    String [] instruments;
+    ArrayAdapter<String> arrayAdapter;
+    Map<String, Float> map;
+    String goodsName = "guitar";
+    EditText userName;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        createSpinner();
+        createMap();
+    }
+
+    public void increaseQuantity(View view) {
+        TextView quantityView = findViewById(R.id.quantity_text_view);
+        quantity++;
+        quantityView.setText(Integer.toString(quantity));
+        setNewPrice();
+
+    }
+    public void decreaseQuantity(View view) {
+        if (quantity == 0) return;
+        TextView quantityView = findViewById(R.id.quantity_text_view);
+        quantity--;
+        quantityView.setText(Integer.toString(quantity));
+        setNewPrice();
+    }
+
+    public void addToCart(View view) {
+        userName = findViewById(R.id.nameEditText);
+        Order order = new Order(userName.getText().toString(), goodsName, quantity, getNewPrice());
+        Log.d("print order", order.toString());
+        Intent orderIntent = new Intent(MainActivity.this, OrderActivity.class);
+        orderIntent.putExtra("userName", order.getUserName());
+
+        startActivity(orderIntent);
+    }
+
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        goodsName = spinner.getSelectedItem().toString();
+        setNewPrice();
+        setGoodsView();
+    }
+
+
+    float getNewPrice() {
+        return map.get(goodsName) * quantity;
+    }
+    void createMap() {
+        map = Map.of("guitar", 500.00f, "drums", 1000.00f, "piano", 1200.00f);
+    }
+
+    //************************* SET VIEW ELEMENTS *************************************
+    void setNewPrice() {
+        TextView priceView = findViewById(R.id.price);
+        priceView.setText(Float.toString(getNewPrice()));
+    }
+    void createSpinner() {
+        spinner = findViewById(R.id.spinner);
+        spinner.setOnItemSelectedListener(this);
+        instruments = new String[]{"guitar", "drums", "piano"};
+        arrayAdapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, instruments);
+        arrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(arrayAdapter);
+    }
+    void setGoodsView() {
+        ImageView goodsImageView = findViewById(R.id.goodsImageView);
+        switch (goodsName) {
+            case "guitar" -> {
+                goodsImageView.setImageResource(R.drawable.guitar);
+            }
+            case "drums" -> {
+                goodsImageView.setImageResource(R.drawable.drums);
+            }
+            case "piano" -> {
+                goodsImageView.setImageResource(R.drawable.piano);
+            }
+        }
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+        goodsName = instruments[0];
+
+    }
+}
---
+++ MusicStore/app/src/main/java/com/example/musicstore/Order.java
@@ -0,0 +1,61 @@
+package com.example.musicstore;
+
+import androidx.annotation.NonNull;
+
+public class Order {
+    private String userName;
+    private String goodsName;
+    private int quantity;
+    private float orderPrice;
+
+    public Order(String userName, String goodsName, int quantity, float orderPrice) {
+        this.userName = userName;
+        this.goodsName = goodsName;
+        this.quantity = quantity;
+        this.orderPrice = orderPrice;
+    }
+    public Order() {}
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getGoodsName() {
+        return goodsName;
+    }
+
+    public void setGoodsName(String goodsName) {
+        this.goodsName = goodsName;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public float getOrderPrice() {
+        return orderPrice;
+    }
+
+    public void setOrderPrice(float orderPrice) {
+        this.orderPrice = orderPrice;
+    }
+
+    @NonNull
+    @Override
+    public String toString() {
+        return "Order{" +
+                "userName='" + userName + '\'' +
+                ", goodsName='" + goodsName + '\'' +
+                ", quantity=" + quantity +
+                ", orderPrice=" + orderPrice +
+                '}';
+    }
+}
